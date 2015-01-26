7f810b14cdd5a45df6a68897e74f2f24a0b6849b
brettlangdon <brett@blangdon.com>
2015-01-25 20:15:20 -0500

    update tests for flatten result object

--- test/index.js
+++ test/index.js
@@ -10,54 +10,54 @@ describe('docast', function(){
 
             var wrapper = comments[0];
             assert.ok(~wrapper.doc.indexOf('wrapper'));
-            assert.equal(wrapper.data.name, null);
-            assert.deepEqual(wrapper.data.params, ['global']);
-            assert.deepEqual(wrapper.data.returns, []);
-            assert.deepEqual(wrapper.data.raises, []);
+            assert.equal(wrapper.name, null);
+            assert.deepEqual(wrapper.params, ['global']);
+            assert.deepEqual(wrapper.returns, []);
+            assert.deepEqual(wrapper.raises, []);
 
             var func1 = comments[1];
             assert.ok(~func1.doc.indexOf('func1'));
-            assert.equal(func1.data.name, 'func1');
-            assert.deepEqual(func1.data.params, ['arg1', 'arg2']);
-            assert.deepEqual(func1.data.returns, ['string']);
-            assert.deepEqual(func1.data.raises, ['Exception']);
+            assert.equal(func1.name, 'func1');
+            assert.deepEqual(func1.params, ['arg1', 'arg2']);
+            assert.deepEqual(func1.returns, ['string']);
+            assert.deepEqual(func1.raises, ['Exception']);
 
             var func2 = comments[2];
             assert.ok(~func2.doc.indexOf('func2'));
-            assert.equal(func2.data.name, 'func2');
-            assert.deepEqual(func2.data.params, ['arg1', 'arg2']);
-            assert.deepEqual(func2.data.returns, [5]);
-            assert.deepEqual(func2.data.raises, []);
+            assert.equal(func2.name, 'func2');
+            assert.deepEqual(func2.params, ['arg1', 'arg2']);
+            assert.deepEqual(func2.returns, [5]);
+            assert.deepEqual(func2.raises, []);
 
             var class1 = comments[3];
             assert.ok(~class1.doc.indexOf('class1'));
-            assert.equal(class1.data.name, 'class1');
-            assert.deepEqual(class1.data.params, ['options']);
-            assert.deepEqual(class1.data.returns, []);
-            assert.deepEqual(class1.data.raises, []);
+            assert.equal(class1.name, 'class1');
+            assert.deepEqual(class1.params, ['options']);
+            assert.deepEqual(class1.returns, []);
+            assert.deepEqual(class1.raises, []);
 
             var class1_method1 = comments[4];
             assert.ok(~class1_method1.doc.indexOf('class1.method1'));
-            assert.equal(class1_method1.data.name, 'method1');
-            assert.equal(class1_method1.data.class, 'class1');
-            assert.deepEqual(class1_method1.data.params, ['arg1']);
-            assert.deepEqual(class1_method1.data.returns, []);
-            assert.deepEqual(class1_method1.data.raises, []);
+            assert.equal(class1_method1.name, 'method1');
+            assert.equal(class1_method1.class, 'class1');
+            assert.deepEqual(class1_method1.params, ['arg1']);
+            assert.deepEqual(class1_method1.returns, []);
+            assert.deepEqual(class1_method1.raises, []);
 
             var class2 = comments[5];
             assert.ok(~class2.doc.indexOf('class2'));
-            assert.equal(class2.data.name, 'class2');
-            assert.deepEqual(class2.data.params, ['options']);
-            assert.deepEqual(class2.data.returns, []);
-            assert.deepEqual(class2.data.raises, []);
+            assert.equal(class2.name, 'class2');
+            assert.deepEqual(class2.params, ['options']);
+            assert.deepEqual(class2.returns, []);
+            assert.deepEqual(class2.raises, []);
 
             var class2_method1 = comments[6];
             assert.ok(~class2_method1.doc.indexOf('class2.method1'));
-            assert.equal(class2_method1.data.name, 'method1');
-            assert.equal(class2_method1.data.class, 'class2');
-            assert.deepEqual(class2_method1.data.params, ['arg1']);
-            assert.deepEqual(class2_method1.data.returns, []);
-            assert.deepEqual(class2_method1.data.raises, []);
+            assert.equal(class2_method1.name, 'method1');
+            assert.equal(class2_method1.class, 'class2');
+            assert.deepEqual(class2_method1.params, ['arg1']);
+            assert.deepEqual(class2_method1.returns, []);
+            assert.deepEqual(class2_method1.raises, []);
         });
     });
 });
